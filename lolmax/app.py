89e5dc37021b93e401655bd7c6f13ac494ef58f3
David Hughes <d@vidhughes.com>
2024-06-22 22:06:09 -0700

    Content negotiation and curl examples

--- lolmax/app.py
+++ lolmax/app.py
@@ -10,17 +10,38 @@ from lolmax import models
 app = Flask(__name__)
 
 
+# curl -X POST localhost:8000 \
+#   -H "Accept: text/plain" \
+#   -H "Content-Type: application/json" \
+#   --date '{"model": "perplexity", "messages": [{"role": "user", "content": "hey, what's up?"}]}'
 @app.route("/chat", methods=["POST"])
 def chat():
     prompt = ChatPrompt.extract(request.json)
-    return Response(prompt.stream(), mimetype="application/jsonlines+json")
+    mimetype = request.accept_mimetypes.best_match([
+        "application/jsonlines+json",
+        "application/json",
+        "text/plain",
+    ])
+    if mimetype == "text/plain":
+        return Response(prompt.stream_text(), mimetype="text/plain")
+    else:
+        # default, but should implies any of: ["application/jsonlines+json", "application/json"]
+        return Response(prompt.stream_ndjson(),
+                        mimetype="application/jsonlines+json")
 
 
-@app.route("/invoke")
+@app.route("/invoke", methods=["POST"])
 def invoke():
-    client_prompt = ChatPrompt.extract(request.json)
-    result = client_prompt.client.invoke(client_prompt.messages)
-    return Response(result.content, mimetype="text/plain")
+    prompt = ChatPrompt.extract(request.json)
+    result = prompt.invoke()
+    mimetype = request.accept_mimetypes.best_match([
+        "application/json",
+        "text/plain",
+    ])
+    if mimetype == "text/plain":
+        return Response(result["content"], mimetype="text/plain")
+    else:
+        return Response(json.dumps(result), mimetype="application/json")
 
 
 @dataclass
@@ -34,30 +55,42 @@ class ChatPrompt:
         client_factory = initialize_models().get(self.model)
         return client_factory()
 
-    def stream(self):
+    def invoke(self):
+        result = self.client.invoke(self.messages)
+        return {
+            "id": result.id,
+            "content": result.content,
+        }
+
+    def stream_objects(self):
         try:
             for chunk in self.client.stream(self.messages):
-                serializable_chunk = {
+                yield {
                     "id": chunk.id,
                     "content": chunk.content,
                 }
-                yield f"{json.dumps(serializable_chunk)}\n"
         except pydantic.v1.error_wrappers.ValidationError as e:
-            serializable_error = {
+            yield {
                 "id":
                 "error",
                 "content":
                 f"An error occurred while generating a response:\n{json.dumps(e.errors())}",
             }
-            yield f"{json.dumps(serializable_error)}\n"
         except Exception as e:
             __import__('ipdb').set_trace()
-            serializable_error = {
+            yield {
                 "content":
                 f"An error occurred while generating a response:\n{e.message}",
                 "id": "error",
             }
-            yield f"{json.dumps(serializable_error)}\n"
+
+    def stream_ndjson(self):
+        for object in self.stream_objects():
+            yield f"{json.dumps(object)}\n"
+
+    def stream_text(self):
+        for object in self.stream_objects():
+            yield object["content"]
 
     @property
     def converted_messages(self):
